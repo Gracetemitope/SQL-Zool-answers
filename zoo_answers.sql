a40ca2814fd80aeec26068a1ee413e25a3790dbf
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-02-13 04:25:53 -0800

    sql zoo answer

--- zoo_answers.sql
+++ zoo_answers.sql
@@ -91,3 +91,18 @@ SELECT DISTINCT yr
    AND yr NOT IN (SELECT yr FROM nobel
                    WHERE subject='Peace')
 
+
+
+-- SELECT IN SELECT
+
+1.
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+
+2.
+SELECT name FROM world
+  WHERE continent = 'Europe' AND gdp/population >
+     (SELECT gdp/population FROM world
+      WHERE name='United Kingdom')
